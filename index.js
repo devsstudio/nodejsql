fc7ef2c98e4d31d806872c8838072f5b22763fad
Jean Paul Perea <jeanpaper@gmail.com>
2021-09-19 19:27:30 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var NodeJSQList = require("./lib/NodeJSQList");
+
+exports.getList = function (connectionParams, baseParams) {
+  return new NodeJSQList(connectionParams, baseParams);
+};
+
+exports.printMsg = function () {
+  return "This is a message from the demo package";
+};
